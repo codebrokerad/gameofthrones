f61dc79910c2f6ca6dd305ec2424245f9ee5e873
Emma Northridge <emmanorthridge1@gmail.com>
2020-10-26 17:38:55 +0100

    Updated latest HTML and Javascript

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const myGameArea = {
-    canvas: document.createElement('canvas'),
-    start: function () {
-      this.canvas.width = 480;
-      this.canvas.height = 270;
-      this.context = this.canvas.getContext('2d');
-      document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    },
-  };
-  
+  canvas: document.createElement("canvas"),
+  start: function () {
+    this.canvas.width = 480;
+    this.canvas.height = 270;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+  },
+};
+class Component {
+  constructor(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+  }
+  update() {
+    const ctx = myGameArea.context;
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+const snake = new Component(30, 30, "red", 0, 110);
